3313285cd0e7c51f99a2cf9789a6ac4c1fbec783
Antonio Leiva <antonioleivag@gmail.com>
2021-02-11 12:16:09 +0100

    Enums: Create an enum to identify the Article type

--- src/main/kotlin/com/antonioleiva/kotlintraining/Article.kt
+++ src/main/kotlin/com/antonioleiva/kotlintraining/Article.kt
@@ -3,5 +3,11 @@ package com.antonioleiva.kotlintraining
 data class Article(
     val title: String,
     val content: String,
-    val slug: String = title.toSlug()
-)
+    val slug: String = title.toSlug(),
+    val type: Type
+) {
+    enum class Type {
+        TEXT,
+        VIDEO
+    }
+}
--- src/main/kotlin/com/antonioleiva/kotlintraining/getArticles.kt
+++ src/main/kotlin/com/antonioleiva/kotlintraining/getArticles.kt
@@ -3,10 +3,12 @@ package com.antonioleiva.kotlintraining
 fun getArticles(): List<Article> = listOf(
     Article(
         title = "Reactor Bismuth is out",
-        content = "dolor sit amet"
+        content = "dolor sit amet",
+        type = Article.Type.TEXT
     ),
     Article(
         title = "Reactor Aluminium has landed",
-        content = "dolor sit amet"
+        content = "dolor sit amet",
+        type = Article.Type.VIDEO
     )
 )
