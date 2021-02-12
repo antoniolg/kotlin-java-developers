50d6f2a00cd5081d2312d4123fb2d684b7668b7c
Antonio Leiva <antonioleivag@gmail.com>
2021-02-12 11:44:19 +0100

    Objects: Use an object for ArticlesRepository

--- src/main/kotlin/com/antonioleiva/kotlintraining/HtmlController.kt
+++ src/main/kotlin/com/antonioleiva/kotlintraining/HtmlController.kt
@@ -8,18 +8,16 @@ import org.springframework.web.bind.annotation.PathVariable
 @Controller
 class HtmlController {
 
-    private val repository by lazy { ArticlesRepository() }
-
     @GetMapping("/")
     fun blog(model: Model): String {
         model["title"] = "Blog"
-        model["articles"] = repository.getArticles().map { it.render() }
+        model["articles"] = ArticlesRepository.getArticles().map { it.render() }
         return "blog"
     }
 
     @GetMapping("/article/{slug}")
     fun article(@PathVariable slug: String, model: Model): String {
-        requireNotNull(repository.findArticleBySlug(slug)).let { article ->
+        requireNotNull(ArticlesRepository.findArticleBySlug(slug)).let { article ->
             val renderedArticle = article.render(::renderTitle)
             model["title"] = article.title
             model["article"] = renderedArticle
@@ -30,7 +28,7 @@ class HtmlController {
 
     @GetMapping("/article/{slug}/like")
     fun likeArticle(@PathVariable slug: String, model: Model): String {
-        requireNotNull(repository.findArticleBySlug(slug)).let {
+        requireNotNull(ArticlesRepository.findArticleBySlug(slug)).let {
             it.likes++
             return article(slug, model)
         }
